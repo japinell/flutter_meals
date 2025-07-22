e01b7544a626ba53619aba3958da665e38dc5cf2
Jose A. Pinell <japinell@yahoo.com>
2025-07-21 17:32:02 -0700

    Added MealItem widget

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import "package:flutter_meals/models/meal.dart";
+import 'package:flutter_meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -11,7 +12,7 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (ctx, idx) => Text(meals[idx].title),
+      itemBuilder: (ctx, idx) => MealItem(meal: meals[idx]),
     );
 
     if (meals.isEmpty) {
