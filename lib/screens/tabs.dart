7c3ac3dfe7b34954958a13e3f41f2e80fc1ddd72
Jose A. Pinell <japinell@yahoo.com>
2025-08-03 13:36:06 -0700

    Pass favorite meals list to Meals Screen

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -39,7 +39,7 @@ class _TabsScreenState extends State<TabsScreen> {
 
     if (_selectedPageIndex == 1) {
       activePage = MealsScreen(
-        meals: [],
+        meals: _favoriteMeals,
         onToggleFavorite: _toggleFavoriteMeal,
       );
       activePageTitle = "Favorites";
