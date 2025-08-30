0093375361e9e96580a3c37c545e7ce0bcef8651
Jose A. Pinell <japinell@yahoo.com>
2025-08-30 14:26:01 -0700

    Wired the meals drawer to the filters screen

---
+++ lib/screens/filters.dart
@@ -0,0 +1,59 @@
+import "package:flutter/material.dart";
+import "package:flutter_meals/screens/tabs.dart";
+import "package:flutter_meals/widgets/meal_drawer.dart";
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() {
+    return _FiltersScreenState();
+  }
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Preferences")),
+      drawer: MealDrawer(
+        onSelectScreen: (id) {
+          Navigator.of(context).pop();
+          if (id == "meals") {
+            Navigator.of(
+              context,
+            ).push(MaterialPageRoute(builder: (context) => TabsScreen()));
+          }
+        },
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _glutenFree,
+            onChanged: (isGlutenFree) {
+              setState(() {
+                _glutenFree = isGlutenFree;
+              });
+            },
+            title: Text(
+              "Gluten-free",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            subtitle: Text(
+              "Only include gluten-free meals.",
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: EdgeInsets.only(left: 34, right: 22),
+          ),
+        ],
+      ),
+    );
+  }
+}
