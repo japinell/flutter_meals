5c3aa6bb31ea8373417adfc8b2f771471f5bff9f
Jose A. Pinell <japinell@yahoo.com>
2025-08-30 19:52:41 -0700

    Added a filtered meals provider to filters provider, and refactored the app to use in tabs screen

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,4 +1,5 @@
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:flutter_meals/providers/meals_provider.dart";
 
 enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
 
@@ -24,3 +25,24 @@ final filtersProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
       (ref) => FiltersNotifier(),
     );
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final mealPreferences = ref.watch(filtersProvider);
+
+  return meals.where((meal) {
+    if (mealPreferences[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (mealPreferences[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (mealPreferences[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    if (mealPreferences[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
