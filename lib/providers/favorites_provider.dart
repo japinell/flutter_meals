526ffb4281f84e659fc2448d2d9d133acf2f523e
Jose A. Pinell <japinell@yahoo.com>
2025-08-30 18:40:38 -0700

    Created state notifier for favorite meals

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,21 @@
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:flutter_meals/models/meal.dart";
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleFavoriteMeal(Meal meal) {
+    final toggleFavorite = state.contains(meal);
+
+    if (toggleFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+      return FavoriteMealsNotifier();
+    });
